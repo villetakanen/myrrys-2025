238721d120d4f6f185ac8c82da3737f12e2cc6d0
Ville Takanen <vitkukissa@gmail.com>
2025-02-18 20:21:43 +0200

    fix: correct canonical url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://myrrys-2025.netlify.app/",
+  integrations: [sitemap()],
+});
